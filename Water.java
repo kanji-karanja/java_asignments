95a3fe19c1f522e2da1236983ef125fab853f27a
kanji-karanja <karimkanji101@gmail.com>
2017-05-25 20:10:49 +0300

    Added try catch syntax

--- Water.java
+++ Water.java
@@ -10,10 +10,22 @@ public class Water{
         }
     }
     static void intro(){
-        int typeOfCustomer;
-        System.out.println("Please choice choose the type of customer you are:\n1. Metered\n2.Unmetered");
+        int typeOfCustomer=0;
+        System.out.println("Please choose the type of customer you are:\n1. Metered\n2.Unmetered");
         Scanner input = new Scanner(System.in);
+        Scanner input2 = new Scanner(System.in);
+        try{
         typeOfCustomer = input.nextInt();
+        System.out.println("\n");
+        }
+        catch(java.util.InputMismatchException e){
+            System.out.println("\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+            System.out.println("Sorry! The input is invalid. Please put a number only");
+            System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n\nTry here again:");
+            typeOfCustomer = input2.nextInt();
+            System.out.println("\n");
+
+        }
         switch(typeOfCustomer){
             case 1:{metered();};break;
             case 2:{unmetered();};break;
@@ -25,11 +37,22 @@ public class Water{
 
     }
    static void metered(){
-    int typeOfMetered;
+    int typeOfMetered=0;
     System.out.println("Dear customer, what type of metered customer are you?");
     System.out.println("1.Normal Customer\n2.Industry customer");
+    Scanner input = new Scanner(System.in);
     Scanner input2 = new Scanner(System.in);
+    try{
     typeOfMetered= input2.nextInt();
+    }
+    catch(java.util.InputMismatchException e){
+           System.out.println("\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+            System.out.println("Sorry! The input is invalid. Please put a number only");
+            System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n\nTry here again:");
+            typeOfMetered = input.nextInt();
+            System.out.println("\n");
+         
+    }
     switch(typeOfMetered){
         case 1:{normalMetered();};break;
         case 2:{industryMetered();};break;
@@ -39,11 +62,21 @@ public class Water{
     }
     }
    static void unmetered(){
-    int numOfUnits;
+    int numOfUnits=0;
     double discount1,subtotal1;
     System.out.println("Please input the number of units you purchased:");
     Scanner input3 = new Scanner(System.in);
+    Scanner input2 = new Scanner(System.in);
+    try{
     numOfUnits = input3.nextInt();
+    }
+    catch(java.util.InputMismatchException e){
+          System.out.println("\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+            System.out.println("Sorry! The input is invalid. Please put a number only");
+            System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n\nTry here again:");
+            numOfUnits = input2.nextInt();
+        
+    }
     if(numOfUnits>0 && numOfUnits<150){
         subtotal1 = numOfUnits*33;
         if(numOfUnits>100){
@@ -64,26 +97,35 @@ public class Water{
     }
     }
    static void normalMetered(){
-    int numOfUnits;
+    int numOfUnits=0;
     double discount1,subtotal1;
     System.out.println("Please input the number of units you purchased:");
     Scanner input3 = new Scanner(System.in);
+    Scanner input2 = new Scanner(System.in);
+    try{
     numOfUnits = input3.nextInt(); 
+    }
+    catch(java.util.InputMismatchException e){
+       System.out.println("\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+            System.out.println("Sorry! The input is invalid. Please put a number only");
+            System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n\nTry here again:");
+            numOfUnits = input2.nextInt();
+    }
     if(numOfUnits>0 && numOfUnits<150){
         subtotal1 = numOfUnits*33;
         if(numOfUnits>100){
-            discount1=(double)subtotal1*8/100;
+            discount1=(double)subtotal1*1/100;
             subtotal1=subtotal1-discount1;
-            double discount2=(double)subtotal1*1/10;
+            double discount2=(double)subtotal1*8/10;
             subtotal1=subtotal1-discount2;
         }
     paymentMetered(subtotal1);
     }
     else if(numOfUnits>149){
         subtotal1=(149*33)+((numOfUnits-149)*30);
-        discount1=(double)subtotal1*8/100;
+        discount1=(double)subtotal1*1/100;
         subtotal1=subtotal1-discount1;
-        double discount2=(double)subtotal1*1/10;
+        double discount2=(double)subtotal1*8/10;
         subtotal1=subtotal1-discount2;
     paymentMetered(subtotal1);
     }
@@ -106,12 +148,21 @@ public class Water{
     paymentMetered(subtotal1);
     }
     static void paymentMetered(double number){
-    int paymentType;
+    int paymentType=0;
     double discount4,receivedTotal=number,total;
     System.out.println("Howdy! How would you like to pay for your bill?");
     System.out.println("1.Electronically(Use of Credit Card\n2.Manually(cash)");
     Scanner input4 = new Scanner(System.in);
+    Scanner input2 = new Scanner(System.in);
+    try{
     paymentType = input4.nextInt();
+    }
+    catch(java.util.InputMismatchException e){
+         System.out.println("\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+            System.out.println("Sorry! The input is invalid. Please put a number only");
+            System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n\nTry here again:");
+            paymentType = input2.nextInt();
+    }
     switch(paymentType){
         case 1:{
             discount4 = receivedTotal*5/100;
@@ -130,12 +181,21 @@ public class Water{
     continueControl();
     }
     static void paymentUnmetered(double number){
-    int paymentType;
+    int paymentType=0;
     double discount4,receivedTotal=number,total;
     System.out.println("Howdy! How would you like to pay for your bill?");
     System.out.println("1.Electronically(Use of Credit Card\n2.Manually(cash)");
     Scanner input4 = new Scanner(System.in);
+    Scanner input2 = new Scanner(System.in);
+    try{
     paymentType = input4.nextInt();
+    }
+    catch(java.util.InputMismatchException e){
+         System.out.println("\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+            System.out.println("Sorry! The input is invalid. Please put a number only");
+            System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n\nTry here again:");
+            paymentType = input2.nextInt();
+    }
     switch(paymentType){
         case 1:{
             discount4 = receivedTotal*5/100;
@@ -158,7 +218,16 @@ public class Water{
     System.out.println("Do you want to continue?");
     System.out.println("1. Yes \n2. No");
     Scanner input = new Scanner(System.in);
+    Scanner input2 = new Scanner(System.in);
+    try{
     controlUser = input.nextInt();
+    }
+    catch(java.util.InputMismatchException e){
+        System.out.println("\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+            System.out.println("Sorry! The input is invalid. Please put a number only");
+            System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n\nTry here again:");
+            controlUser = input2.nextInt();
+    }
     switch(controlUser){
         case 1:{control=0;};break;
         case 2:{control=1;};break;
